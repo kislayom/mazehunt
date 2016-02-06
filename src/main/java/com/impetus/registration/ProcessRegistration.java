65f25acdc6df7753fd4b4f3e96d5074659430d65
amit <pathak.amit783@gmail.com>
2016-02-07 01:23:16 +0530

    Amit

--- src/main/java/com/impetus/registration/ProcessRegistration.java
+++ src/main/java/com/impetus/registration/ProcessRegistration.java
@@ -1,34 +1,27 @@
-package com.impetus.registration;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.impetus.registration;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-/**
- *
- * @author kislay
- */
 public class ProcessRegistration extends HttpServlet {
 
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
@@ -40,45 +33,17 @@ public class ProcessRegistration extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        Connection con = null;
+        PrintWriter out = response.getWriter();
+        try {
             /* TODO output your page here. You may use following sample code. */
-           String email_id=request.getParameter("form-email");
-           out.write(email_id);
-           
-           ////////////
-           	final String username = "mazehunt@gmail.com";
-		final String password = "0jb1a11a";
-
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
-
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		  });
-
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("mazehunt@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(email_id));
-			message.setSubject("Welcome to MazeHunt");
-			message.setText("Your Password is xxx fuck you.");
-
-			Transport.send(message);
-                }catch(Exception exc){
-                    out.write(exc.toString());
-                }
-
-			out.write("Done");
-           //////////////////
-           
+            String emailid = request.getParameter("form-email");
+            System.out.println("emailid>>>>>>>>>>>>>>>>>>>>>>"+emailid);
+         
+            out.println(emailid);
+        } catch (Exception exc) {
+            out.println(exc.toString());
+        } finally {
            
         }
     }
