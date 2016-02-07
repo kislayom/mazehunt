8a8ca0e0b5c8445e84d703603a43af45ab39f0f0
Kislay Sinha <kislay.osho@gmail.com>
2016-02-07 11:25:56 +0530

    kislay

--- src/main/java/com/utils/DbConnection.java
+++ src/main/java/com/utils/DbConnection.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class DbConnection {
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://192.168.1.102:3306/mazehunt";
+    static final String DB_URL = "jdbc:mysql://104.168.163.65:3306/mazehunt";
 
     //  Database credentials
     static final String USER = "amit";
